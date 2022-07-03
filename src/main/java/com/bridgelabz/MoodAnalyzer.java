9fdc6a618b0ef3f284f7105e27521dc76b27e3a2
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-07-03 12:48:37 +0530

    Added: Mood analyzer

---
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+public class MoodAnalyzer {
+
+        public static String analyseMood(String message) {
+            if (message.toLowerCase().contains("sad")) {
+                return "Sad";
+            } else if (message.toLowerCase().contains("happy")) {
+                return "Happy";
+            }
+            return null;
+        }
+
+        public static void main(String[] args) {
+            String mood = MoodAnalyzer.analyseMood("User is Happy");
+            System.out.println(mood);
+            mood = MoodAnalyzer.analyseMood("User is sad");
+            System.out.println(mood);
+        }
+    }
+
---
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assert.assertTrue(true);
+    }
+}
