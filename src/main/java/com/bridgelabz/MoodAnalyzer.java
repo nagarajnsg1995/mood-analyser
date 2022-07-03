4a431212c7e6f32968531d1fbb4f70400ca5fdba
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-07-03 13:41:56 +0530

    Added: null mood would throws moodanalysis exception

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,33 +1,37 @@
 package com.bridgelabz;
 
-public class MoodAnalyzer {
-    public static String message;
 
-    // Constructor
-    public MoodAnalyzer(String message) {
-        this.message = message;
-        analyseMood();
-    }
+    public class MoodAnalyzer {
+        public static String message;
 
-    public static String analyseMood() {
-        try {
-            if (message.toLowerCase().contains("sad")) {
-                return "Sad";
-            } else {
-                return "Happy";
+        public MoodAnalyzer() {
+        }
+
+        // Constructor
+        public MoodAnalyzer(String message) throws MoodAnalysisException {
+            this.message = message;
+            analyseMood();
+        }
+
+        //Getter
+        public String getMessage() {
+            return message;
+        }
+
+        //Setter
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public static String analyseMood() throws MoodAnalysisException {
+            try {
+                if (message.toLowerCase().contains("sad")) {
+                    return "Sad";
+                } else {
+                    return "Happy";
+                }
+            } catch (NullPointerException e) {
+                throw new MoodAnalysisException("Entered Invalid mood");
             }
-        } catch (NullPointerException e) {
-            return "Exception Handled";
         }
     }
-
-    //Getter
-    public String getMessage() {
-        return message;
-    }
-
-    //Setter
-    public void setMessage(String message) {
-        this.message = message;
-    }
-}
--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -3,26 +3,35 @@ package com.bridgelabz;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MoodAnalyzerTest {
 
-    @Test
-    public void givenMessage_WhenProper_ShouldReturnSad() {
-        MoodAnalyzer moodanalyser = new MoodAnalyzer("I am in Sad Mood");
-        String actualResult = moodanalyser.analyseMood();
-        Assert.assertEquals("Sad", actualResult);
-    }
 
-    @Test
-    public void givenMessage_WhenProper_ShouldReturnHappy() {
-        MoodAnalyzer moodanalyser = new MoodAnalyzer("I am in happy Mood");
-        String actualResult = moodanalyser.analyseMood();
-        Assert.assertEquals("Happy", actualResult);
-    }
+    public class MoodAnalyzerTest {
 
-    @Test
-    public void givenMessage_Null_ShouldReturnExceptionHandled() {
-        MoodAnalyzer moodanalyser = new MoodAnalyzer(null);
-        String actualResult = moodanalyser.analyseMood();
-        Assert.assertEquals("Exception Handled", actualResult);
+        @Test
+        public void givenMessage_WhenProper_ShouldReturnSad() throws MoodAnalysisException {
+            MoodAnalyzer moodanalyser = new MoodAnalyzer();
+            moodanalyser.setMessage("I am in sad Mood");
+            String actualResult = moodanalyser.analyseMood();
+            Assert.assertEquals("Sad", actualResult);
+        }
+
+        @Test
+        public void givenMessage_WhenProper_ShouldReturnHappy() throws MoodAnalysisException {
+            MoodAnalyzer moodanalyser = new MoodAnalyzer();
+            moodanalyser.setMessage("I am in happy Mood");
+            String actualResult = moodanalyser.analyseMood();
+            Assert.assertEquals("Happy", actualResult);
+        }
+
+        @Test
+        public void givenMessage_Null_ShouldReturnMessage() throws MoodAnalysisException {
+            MoodAnalyzer moodanalyser = new MoodAnalyzer();
+            moodanalyser.setMessage(null);
+            try {
+                String actualResult = moodanalyser.analyseMood();
+                Assert.assertEquals("Entered Invalid Mood ", actualResult);
+            } catch (MoodAnalysisException e) {
+                e.printStackTrace();
+            }
+        }
     }
-}
